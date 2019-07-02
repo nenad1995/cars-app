c4cfda4f63ffab7dcdfbd2805e4ca26c2c781160
nenad1995 <nenad.zoric@yahoo.com>
2019-07-02 19:42:49 +0200

    Zadatak 2

---
+++ src/components/CarList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="car in cars" :key="car.id">
+      <div>
+        <h3>{{ car.brand }}</h3>
+        <h3>Model: {{ car.model }}</h3>
+      </div>
+      <ul>
+        <li>Year: {{ car.year }}</li>
+        <li>Number of doors: {{ car.numberOfDoors }}</li>
+        <li>Maximum speed: {{ car.maxSpeed }}</li>
+        <li>{{ car.isAutomatic ? 'Automatic' : 'Manual' }}</li>
+        <li>Engine: {{ car.engine }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cars: Array
+  }
+}
+</script>
--- src/containers/AppCars.vue
+++ src/containers/AppCars.vue
@@ -1,8 +1,34 @@
 <template>
-  <div>Cars placeholder</div>
+  <div>
+    <car-list :cars="cars"></car-list>
+  </div>
 </template>
 
 <script>
+import CarList from '../components/CarList.vue'
+import { cars } from '../services/car-service'
+
 export default {
+  components: {
+    CarList
+  },
+
+  data () {
+    return {
+      cars: []
+    }
+  },
+
+  beforeRouteEnter (to, from, next) {
+    cars.getAll()
+      .then((response) => {
+        next((vm) => {
+          console.log(response)
+          vm.cars = response.data
+        })
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
 }
 </script>
